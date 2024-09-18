d308084288a373b362ec76b63847f720a461b687
Mohfazam <mohd.sarwar.code@gmail.com>
2024-09-18 21:28:01 +0530

    Web dev week 1.2 assignment 5

---
+++ Web dev/week 1.2/assignment 5/app.js
@@ -0,0 +1,45 @@
+// create a function that takes an Array of objects as Input, and return the user whose age is > 18 and are male(using filters)
+
+
+function vote(arr){
+    // console.log("\nEligible Voters are: ");
+    // let ans = [];
+    // for(let i = 0; i < arr.length; i++){
+    //     if(arr[i].age >= 18 && arr[i].gender === "male"){
+    //         ans.push(arr[i]);
+    //     }
+    // }
+    // return ans;
+
+    return arr.filter(user => user.age >= 18 && user.gender === "male");
+}
+
+let user = [
+    {
+        name: "sarwar",
+        age: 19,
+        gender: "male"
+    }, 
+
+    {
+        name: "John Doe",
+        age: 26,
+        gender: "male"
+    },
+
+    {
+        name: "Jane Doe",
+        age: 29,
+        gender: "female"
+    },
+
+    {
+        name: "Jimmy Doe",
+        age: 15,
+        gender: "male"
+    }
+
+]
+
+let result = vote(user);
+console.log(result);
