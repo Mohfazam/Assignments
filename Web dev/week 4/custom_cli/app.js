99e4632c96f1713511a96b56602de732e5ff7b1d
Mohfazam <mohd.sarwar.code@gmail.com>
2024-10-03 00:38:10 +0530

    week 4 assignment 1

---
+++ Web dev/week 4/custom_cli/app.js
@@ -0,0 +1,26 @@
+const { Command } =  require("commander");
+const program = new Command();
+
+
+program
+  .name("File word counter")
+  .version("1.0.0")
+  .description("CLI file word counter utility");
+
+program.command('count-lines')
+  .description("count the number of lines in a text files")
+  .argument('<file>', 'file to count')
+  .action(function(file){
+
+    const fs = require("fs");
+    const path = require("path");
+
+    let filepath = path.join(__dirname, file);
+    let text = fs.readFileSync(filepath, "utf-8");
+    let count = text.length;
+
+    console.log(`the number of words in the file ${file} is ${count}`);
+  });
+
+
+program.parse();
