6c91954f2f524deefa96a343c8e7196573e618c3
Mohfazam <mohd.sarwar.code@gmail.com>
2024-10-04 23:28:28 +0530

    week 4 assignment 3

--- Web dev/week 4/forked folder/01-middlewares/03-errorcount.js
+++ Web dev/week 4/forked folder/01-middlewares/03-errorcount.js
@@ -10,6 +10,8 @@ let errorCount = 0;
 // 1. Ensure that if there is ever an exception, the end user sees a status code of 404
 // 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
 
+
+
 app.get('/user', function(req, res) {
   throw new Error("User not found");
   res.status(200).json({ name: 'john' });
@@ -23,4 +25,11 @@ app.get('/errorCount', function(req, res) {
   res.status(200).json({ errorCount });
 });
 
+//error handling middleware
+app.use(function(err, req, res, next){
+  res.status(404).send({});
+  errorCount++;
+});
+
+app.listen(3000);
 module.exports = app;
