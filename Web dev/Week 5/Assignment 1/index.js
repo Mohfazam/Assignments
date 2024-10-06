7125becac76bdd8a62dadc81ab0c1550d66997fa
Mohfazam <mohd.sarwar.code@gmail.com>
2024-10-06 19:44:32 +0530

    week 5 assignment 1

---
+++ Web dev/Week 5/Assignment 1/index.js
@@ -0,0 +1,58 @@
+const express = require("express");
+
+const app = express();
+
+function add(a,b){
+    return Number(a)+Number(b);
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b;
+}
+
+app.get("/add", function(req, res){
+    let x = req.query.a;
+    let y = req.query.b;
+    let ans = add(x, y);
+    res.status(200).json({
+        msg: `the sum is ${ans}`
+    })
+});
+
+app.get("/subtract", function(req, res){
+    let x = req.query.a;
+    let y = req.query.b;
+    let ans = subtract(x, y);
+    res.status(200).json({
+        msg: `the subtraction is ${ans}`
+    })
+});
+
+app.get("/multiply", function(req, res){
+    let x = req.query.a;
+    let y = req.query.b;
+    let ans = multiply(x, y);
+    res.status(200).json({
+        msg: `the multiply is ${ans}`
+    })
+});
+
+app.get("/divide", function(req, res){
+    let x = req.query.a;
+    let y = req.query.b;
+    let ans = divide(x, y);
+    res.status(200).json({
+        msg: `the divide is ${ans}`
+    })
+});
+
+
+app.listen(3000);
